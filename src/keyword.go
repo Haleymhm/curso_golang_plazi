b3875a54274932f920e8e8eb7deb88b0204126cb
Haleym Hidalgo <haleymhm@gmail.com>
2022-06-29 19:22:07 -0400

    KyeWords

--- src/keyword.go
+++ src/keyword.go
@@ -9,13 +9,14 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 		if i == 4 {
-			fmt.Println("Es 4")
+			fmt.Println("Soy el 4")
+			fmt.Println("despues de continue")
 			continue
 		}
 
 		// Break
-		if i == 4 {
-			fmt.Println("Break")
+		if i == 8 {
+			fmt.Println("Se detiene el ciclo con un break antes de llagar a 10")
 			break
 		}
 	}
